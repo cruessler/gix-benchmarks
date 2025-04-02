52f4e088f1da01c9d85563448a982fad2e45a5ba
Christoph Rüßler <christoph.ruessler@mailbox.org>
2025-04-02 10:52:02 +0200

    Traverse history using `git2`

---
+++ src/git_backend.rs
@@ -0,0 +1,72 @@
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashSet},
+};
+
+use git2::{Commit, Oid, Repository};
+
+/// This is a simplified version of:
+/// https://github.com/gitui-org/gitui/blob/156381155e12d89b39e657f07c8dac557271d24f/asyncgit/src/sync/logwalker.rs
+
+struct TimeOrderedCommit<'a>(Commit<'a>);
+
+impl Eq for TimeOrderedCommit<'_> {}
+
+impl PartialEq for TimeOrderedCommit<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.time().eq(&other.0.time())
+    }
+}
+
+impl PartialOrd for TimeOrderedCommit<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for TimeOrderedCommit<'_> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.time().cmp(&other.0.time())
+    }
+}
+
+pub struct GitLogWalker<'a> {
+    commits: BinaryHeap<TimeOrderedCommit<'a>>,
+    visited: HashSet<Oid>,
+}
+
+impl<'a> GitLogWalker<'a> {
+    pub fn new(repo: &'a Repository) -> Self {
+        let commit = repo.head().unwrap().peel_to_commit().unwrap();
+
+        let mut commits = BinaryHeap::with_capacity(10);
+        commits.push(TimeOrderedCommit(commit));
+
+        Self {
+            commits,
+            visited: HashSet::with_capacity(1000),
+        }
+    }
+
+    pub fn read(&mut self, out: &mut Vec<Oid>) -> usize {
+        let mut count = 0_usize;
+
+        while let Some(commit) = self.commits.pop() {
+            for parent in commit.0.parents() {
+                self.visit(parent);
+            }
+
+            out.push(commit.0.id());
+
+            count += 1;
+        }
+
+        count
+    }
+
+    fn visit(&mut self, commit: Commit<'a>) {
+        if self.visited.insert(commit.id()) {
+            self.commits.push(TimeOrderedCommit(commit));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use crate::git_backend::GitLogWalker;
+use clap::{Parser, ValueEnum};
+use git2::Repository;
+
+mod git_backend;
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Library {
+    /// Use `libgit2` for running the benchmark.
+    Git,
+    /// Use `gitoxide` for running the benchmark.
+    Gix,
+}
+
+#[derive(Debug, clap::Subcommand)]
+enum Subcommands {
+    /// Benchmark traversing a repository’s full history.
+    Log {
+        #[arg(value_enum)]
+        library: Library,
+    },
+}
+
+#[derive(Debug, clap::Parser)]
+#[clap(name = "gix-benchmarks")]
+struct Args {
+    #[clap(subcommand)]
+    pub cmd: Subcommands,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse_from(std::env::args_os());
+
+    match args.cmd {
+        Subcommands::Log { library } => match library {
+            Library::Git => {
+                let repo = Repository::open_from_env().unwrap();
+
+                println!(
+                    "walking the history of {workdir:?}, counting the number of cs in commit ids",
+                    workdir = repo.workdir().unwrap()
+                );
+
+                let mut walker = GitLogWalker::new(&repo);
+                let mut commits = Vec::new();
+                let mut number_of_commits = 0;
+                let mut number_of_cs = 0;
+
+                while walker.read(&mut commits) > 0 {
+                    number_of_commits += commits.iter().count();
+                    number_of_cs += commits.iter().fold(0, |acc, commit| {
+                        acc + commit
+                            .to_string()
+                            .chars()
+                            .filter(|char| *char == 'c')
+                            .count()
+                    });
+
+                    commits.clear();
+                }
+
+                println!("number of commits traversed: {number_of_commits}");
+                println!("number of cs in all commit ids traversed: {number_of_cs}");
+            }
+            Library::Gix => todo!(),
+        },
+    }
 }
